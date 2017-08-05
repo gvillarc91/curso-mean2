5e2a5e28a85adf01b3d073307c0f5b55d9dcb537
gaston <gastonnicolasvillar@gmail.com>
2017-08-05 19:24:32 -0300

    SaveArtist

--- controllers/artist.js
+++ controllers/artist.js
@@ -12,7 +12,30 @@ function getArtist(req, res){
 	res.status(200).send({message: 'Método getArtist del controlador Artist.js'});
 }
 
+function saveArtist(req, res){
+	
+	var params = req.body;
+
+	var artist = new Artist();
+	artist.name = params.name;
+	artist.description = params.description;
+	artist.image = 'null';
+
+	artist.save((err, artistStored)=>{
+		if(err){
+			res.status(500).send({message: 'Error al guardar el artista'});
+		}else{
+			if(!artistStored){
+				res.status(404).send({message: 'El artista no ha sido guardado'});
+			}else{
+				res.status(200).send({artist: artistStored});
+			}
+		}
+	});
+}
+
 module.exports = {
-	getArtist
+	getArtist,
+	saveArtist
 };
 
