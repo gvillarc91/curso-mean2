37518aef7f969acfd782e6e39ef77d1ef75d85f1
gaston <gastonnicolasvillar@gmail.com>
2017-08-05 20:23:43 -0300

    Delete Artist

--- controllers/artist.js
+++ controllers/artist.js
@@ -3,6 +3,7 @@
 //Incluyo sistema de ficheros
 var fs = require('fs');
 var path = require('path');
+var mongoosePaginate = require('mongoose-pagination');
 
 var Album = require('../models/album');
 var Artist = require('../models/artist');
@@ -50,9 +51,105 @@ function saveArtist(req, res){
 	});
 }
 
+function getArtists(req, res){
+
+	if(req.params.page){
+
+		var page = req.params.page;
+	}else{
+		var page = 1;
+	}
+
+	var itemsPage =  3;
+
+	Artist.find().sort('name').paginate(page, itemsPage, function(err, artists, total){
+		
+		if(err){
+			res.status(500).send({message: 'Error en la petición'});
+		}else{
+			if(!artists){
+				res.status(404).send({message: 'No hay artistas'});
+			}else{
+				return res.status(200).send({
+					total_items: total,
+					artists: artists
+				});
+			}
+		}
+
+	});
+
+}
+
+function updateArtist(req, res){
+
+	var artistId = req.params.id;
+	var update = req.body;
+
+	Artist.findByIdAndUpdate(artistId, update, (err, artistUpdated)=>{
+		
+		if(err){
+			res.status(500).send({message: 'Error al actualizar el artista'});
+		}else{
+			if(!artistUpdated){
+				res.status(404).send({message: 'No se ha podido actualizar el artista'});
+			}else{
+				res.status(200).send({artist: artistUpdated });
+			}
+		}
+	});
+}
+
+function deleteArtist(req, res){
+
+	var artistId = req.params.id;
+
+	Artist.findByIdAndRemove(artistId, (err, artistRemoved) => {
+		
+		if(err){
+			res.status(500).send({message: 'Error al eliminar el artista'});
+		}else{
+			if(!artistRemoved){
+				res.status(404).send({message: 'No se ha podido eliminar el artista'});
+			}else{
+				res.status(404).send({artistRemoved});
+				
+				Album.find({ artist: artistRemoved._id}).remove((err, albumRemoved) => {
+
+					if(err){
+						res.status(500).send({message: 'Error al eliminar el album'});
+					}else{
+						if(!albumRemoved){
+							res.status(404).send({message: 'No se ha podido eliminar el album'});
+						}else{
+							
+							Song.find({ album: albumRemoved._id}).remove((err, songRemoved) => {
+
+									if(err){
+										res.status(500).send({message: 'Error al eliminar la cancion'});
+									}else{
+										if(!songRemoved){
+											res.status(404).send({message: 'No se ha podido eliminar la cancion'});
+										}else{ res.status(200).send({artistRemoved});}
+									}
+							});
+						}
+					}
+
+				});
+			}
+		}
+	});
+}
+
+
+
 
 module.exports = {
 	getArtist,
-	saveArtist
+	saveArtist,
+	getArtists,
+	updateArtist,
+	deleteArtist
 };
 
