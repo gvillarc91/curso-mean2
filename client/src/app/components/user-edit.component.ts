481f86e3d333acd20a51cddbc4c0041fc48da7af
gaston <gastonnicolasvillar@gmail.com>
2017-08-18 18:17:47 -0300

    Actualizacion de datos del usuario

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -13,18 +13,49 @@ export class UserEditComponent implements OnInit{
 	public user: User;
 	public identity;
 	public token;
+	public alertMessage;
 
 	constructor(
 		private _userService: UserService
 	){
 		this.titulo = 'Actualizar mis datos';
+	
+		//LocalStorage
 		this.identity = this._userService.getIdentity();
         this.token = this._userService.getToken();
+
+        this.user = this.identity;
 
 	}
 
 	ngOnInit(){
-
 		console.log('user-edit.component.ts cargado');
+	}
+	onSubmit(){
+		console.log(this.user);
+
+		this._userService.updateUser(this.user).subscribe(
+			response => {
+
+				if(!response.user){
+					this.alertMessage = 'El usuario no se ha actualizado';
+				}else{
+					//this.user = response.user;
+					localStorage.setItem('identity', JSON.stringify(this.user));
+					document.getElementById("identity_name").innerHTML = this.user.name;
+					this.alertMessage = 'El usuario se ha actualizado correctamente';
+				}
+			},
+			error => {
+                    var errorMessage = <any>error;
+                    if(errorMessage != null){
+                      var body = JSON.parse(error._body);
+                      this.alertMessage = body.message;
+                      console.log(error);
+                    }
+
+                  
+			}
+		);
 	}
 }
