4f9306f665f8d4321351f55163e193ac7935da48
gaston <gastonnicolasvillar@gmail.com>
2017-08-19 00:09:47 -0300

    Agrego upload de la imagen del usuario

--- client/src/app/components/user-edit.component.ts
+++ client/src/app/components/user-edit.component.ts
@@ -57,7 +57,8 @@ export class UserEditComponent implements OnInit{
 							(result: any)=>{
 								this.user.image = result.image;
 								localStorage.setItem('identity', JSON.stringify(this.user));
-
+								let image_path = this.url + 'get-image-user/' +  this.user.image;
+                		        document.getElementById('image-logged').setAttribute('src', image_path);
 								console.log(this.user);
 							}
 
