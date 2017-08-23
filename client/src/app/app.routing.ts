95ad4d2af7c6fd6591a9091d7b926ada41e8210f
gaston <gastonnicolasvillar@gmail.com>
2017-08-23 16:04:39 -0300

    Imagen del artista

--- client/src/app/app.routing.ts
+++ client/src/app/app.routing.ts
@@ -5,12 +5,14 @@ import {Routes, RouterModule} from '@angular/router';
 import {UserEditComponent} from './components/user-edit.component';
 import {ArtistListComponent} from './components/artist-list.component';
 import {ArtistAddComponent } from './components/artist-add.component';
+import { ArtistEditComponent } from './components/artist-edit.component';
 import {HomeComponent} from './components/home.component';
 
 const appRoutes: Routes = [
 	{path: '', component: HomeComponent},
 	{path: 'artistas/:page', component: ArtistListComponent},
 	{path: 'crear-artista', component: ArtistAddComponent},
+	{path: 'editar-artista/:id', component: ArtistEditComponent},
 	{path: 'mis-datos', component: UserEditComponent},
 	{path: '**', component: HomeComponent}
 ];
--- client/src/app/services/artist.service.ts
+++ client/src/app/services/artist.service.ts
@@ -1,7 +1,7 @@
 //Imports basicos que tiene casi cualquier servicio
 
 import { Injectable } from '@angular/core';
-import { Http, Response, Headers} from '@angular/http';
+import { Http, Response, Headers, RequestOptions} from '@angular/http';
 import 'rxjs/add/operator/map';
 import { Observable } from 'rxjs/Observable';
 import {GLOBAL } from './global';
@@ -13,6 +13,56 @@ export class ArtistService{
 
 	constructor(private _http: Http){
 		this.url = GLOBAL.url;
+	}
+
+	getArtists(token, page){
+
+		let headers = new Headers({
+			'Content-Type':'application/json',
+			'Authorization': token
+		});
+
+		let options = new RequestOptions({headers: headers});
+		return this._http.get(this.url + 'artists/' + page, options)
+			.map(res => res.json());
+
+	}
+
+	getArtist(token, id:string){
+
+		let headers = new Headers({
+			'Content-Type':'application/json',
+			'Authorization': token
+		});
+
+		let options = new RequestOptions({headers: headers});
+		return this._http.get(this.url + 'artist/' + id, options)
+			.map(res => res.json());
+
+	}
+
+	editArtist(token, id: string, artist: Artist){
+		let params = JSON.stringify(artist);
+		let headers =  new Headers({
+			'Content-Type':'application/json',
+			'Authorization': token
+		});
+
+		return this._http.put(this.url+'artist/'+ id, params, {headers:headers})
+			.map(res => res.json());
+	}
+
+	deleteArtist(token, id: string){
+
+		let headers = new Headers({
+			'Content-Type':'application/json',
+			'Authorization': token
+		});
+
+		let options = new RequestOptions({headers: headers});
+		return this._http.delete(this.url + 'artists/' + id, options)
+			.map(res => res.json());
+
 	}
 
 	addArtist(token, artist: Artist){
