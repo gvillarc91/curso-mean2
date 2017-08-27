cb33f353874863ed58bcfec4f04bf50b45d54794
gaston <gastonnicolasvillar@gmail.com>
2017-08-26 23:15:50 -0300

    Sin bugs. Fin artistas

--- api/app.js
+++ api/app.js
@@ -21,7 +21,7 @@ res.header('Access-Control-Allow-Headers', "Authorization, X-API-KEY, Origin, X-
 res.header('Access-Control-Allow-Methods', "GET, POST, OPTIONS, PUT, DELETE");
 res.header('Allow', "GET, POST, OPTIONS, PUT, DELETE" );
 
-	next();
+  next();
 });
 
 //Rutas base
